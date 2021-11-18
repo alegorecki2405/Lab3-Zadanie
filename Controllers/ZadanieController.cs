93cccfbb2e2ab0c78d965dad0d73ea624911b74d
Aleksander Gorecki <alegorecki@gmail.com>
2021-11-19 00:03:59 +0100

    Dodaj pliki projektów.

---
+++ Controllers/ZadanieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3_test4.Models;
+
+namespace Lab3_test4.Controllers
+{
+    public class ZadanieController : Controller
+    {
+
+        public IActionResult ImieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ImieForm(ZadanieModel model)
+        {
+            if (ModelState.IsValid && model.Ilosc_Powtorzen>=1 model.Ilosc_Powtorzen<=5)
+            {
+                return View("ShowImie", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/ZadanieModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3_test4.Models
+{
+    public class ZadanieModel
+    {
+        [Display(Name = "Imię")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Ilość powtórzen")]
+        [Required]
+        public int Ilosc_Powtorzen { get; set; }
+
+    }
+}
